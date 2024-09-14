ef509c97a42eed3c87f5261ea8edafa6aa25529c
Joao Victor <jvsdv3@gmail.com>
2024-09-14 17:00:00 -0300

    live kit conf with initial name on modal e join first camera and unmuted

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,140 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+import {
+  ControlBar,
+  GridLayout,
+  LiveKitRoom,
+  ParticipantTile,
+  RoomAudioRenderer,
+  useTracks,
+  useParticipants,
+} from "@livekit/components-react"
+import "@livekit/components-styles"
+import { Track } from "livekit-client"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+export default function Page() {
+  const room = "quickstart-room"
+  const [name, setName] = useState("")
+  const [token, setToken] = useState("")
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [initialAudioState, setInitialAudioState] = useState(false)
+  const [initialVideoState, setInitialVideoState] = useState(false)
+
+  useEffect(() => {
+    const storedName = localStorage.getItem("username")
+    if (storedName) {
+      setName(storedName)
+      generateToken(storedName)
+    } else {
+      setIsDialogOpen(true)
+    }
+  }, [])
+
+  const generateToken = async (username: string) => {
+    try {
+      const resp = await fetch(
+        `/api/get-participant-token?room=${room}&username=${username}`
+      )
+      const data = await resp.json()
+      setToken(data.token)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  const handleNameSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (name.trim() !== "") {
+      const uniqueName = `${name}-${Math.random().toString(36).substr(2, 5)}`
+      localStorage.setItem("username", uniqueName)
+      setName(uniqueName)
+      setIsDialogOpen(false)
+      generateToken(uniqueName)
+    }
+  }
+
+  if (token === "") {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Dialog open={isDialogOpen} onOpenChange={(open) => {
+          if (name.trim() !== "") {
+            setIsDialogOpen(open)
+          }
+        }}>
+          <DialogContent className="text-white">
+            <DialogHeader>
+              <DialogTitle >Digite seu nome:</DialogTitle>
+            </DialogHeader>
+            <form onSubmit={handleNameSubmit}>
+              <div className="grid gap-4 py-4">
+                <Input
+                  id="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  autoFocus
+                />
+              </div>
+              <DialogFooter >
+                <Button type="submit">Entrar</Button>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+        {!isDialogOpen && <div>Carregando...</div>}
+      </div>
+    )
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <LiveKitRoom
+      video={initialVideoState}
+      audio={initialAudioState}
+      token={token}
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      data-lk-theme="default"
+      style={{ height: '100dvh' }}
+    >
+      <MyVideoConference setInitialAudioState={setInitialAudioState} setInitialVideoState={setInitialVideoState} />
+      <RoomAudioRenderer />
+      <ControlBar />
+    </LiveKitRoom>
+  )
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+function MyVideoConference({ setInitialAudioState, setInitialVideoState } :
+  {
+    setInitialAudioState: Dispatch<SetStateAction<boolean>>,
+    setInitialVideoState: Dispatch<SetStateAction<boolean>>
+  }
+) {
+  const tracks = useTracks(
+    [
+      { source: Track.Source.Camera, withPlaceholder: true },
+      { source: Track.Source.ScreenShare, withPlaceholder: false },
+    ],
+    { onlySubscribed: false },
+  )
+  
+  const participants = useParticipants()
+
+  useEffect(() => {
+    if (participants.length <= 1) {
+      setInitialAudioState(true)
+      setInitialVideoState(true)
+    } else {
+      setInitialAudioState(false)
+      setInitialVideoState(false)
+    }
+  }, [participants, setInitialAudioState, setInitialVideoState])
+
+  return (
+    <GridLayout tracks={tracks} style={{ height: 'calc(100vh - var(--lk-control-bar-height))' }}>
+      <ParticipantTile />
+    </GridLayout>
+  )
 }
