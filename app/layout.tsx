93b26788061085d5d64ae50bbd3a41550b461593
Joao Victor <jvsdv3@gmail.com>
2026-03-21 16:39:28 -0300

    back version and speed control vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Reunião de Estudo",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
